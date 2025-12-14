2a8cd747c11a5d8456bde21454c09272baa04c2b
1DECRATE1 <nukegame12@gmail.com>
2025-12-14 21:13:48 +0300

    Complete backend

--- Controllers/Convert.cs
+++ Controllers/Convert.cs
@@ -1,22 +1,44 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+
+public class FormModel
+{
+    public required IFormFile Video {get; set;}
+}
 
 [ApiController]
 [Route("api/[controller]")]
 public class Convert: ControllerBase
 {
-    [HttpGet("AddSubtitle")]
-    public async Task<IActionResult> Subtitle()
+    [HttpPost("AddSubtitle")]
+    public async Task<IActionResult> Subtitle([FromForm] FormModel formData)
     {
+        //Сохранение файла
+        IFormFile file = formData.Video;
+        Console.WriteLine(file);
+        string tempDir = Path.GetTempPath();
+        string path = Path.Combine(tempDir, Guid.NewGuid().ToString() + ".mp4");
+        using(var streamFile = System.IO.File.Create(path))
+        {
+            await file.CopyToAsync(streamFile);
+        }
+
+        //Старт процесса
         var start = new ProcessStartInfo
         {
             FileName = "python",
-            Arguments = "./sp-rec-model/main.py",
             UseShellExecute = false,
             RedirectStandardOutput = true,
             RedirectStandardError = true,
             CreateNoWindow = true
         };
+
+        string scriptFile = "D:/Projects/sp-backend/model/sp-rec-subtitle/main.py";
+
+        start.ArgumentList.Add(scriptFile);
+        start.ArgumentList.Add(path);
+        start.ArgumentList.Add("D:/Projects/sp-backend/model/");
 
         using var process = Process.Start(start) ?? throw new InvalidOperationException("Cannot start process");
         var outTask = process.StandardOutput.ReadToEndAsync();
@@ -24,11 +46,8 @@ public class Convert: ControllerBase
         await process.WaitForExitAsync();
         var stdout = (await outTask).Trim();
         var stderr = (await errTask).Trim();
-
         if (process.ExitCode != 0) return StatusCode(500, $"Exit {process.ExitCode}: {stderr}");
         if (string.IsNullOrEmpty(stdout)) return StatusCode(500, "Empty output from python");
-        if (!System.IO.File.Exists(stdout)) return NotFound($"File not found: {stdout}");
-
         var stream = System.IO.File.OpenRead(stdout);
         return File(stream, "video/mp4", Path.GetFileName(stdout));
     }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .AllowAnyOrigin() // адрес фронтенда
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +23,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
+
 app.UseRouting();
 app.MapControllers();
 app.Run();
+
